708b74d2f77fbbb333304a2a7d5733a01e7bb988
dreamerjackson <zhuimengshaonian04@gmail.com>
2018-10-05 01:20:51 +0800

    jsx

--- README.md
+++ README.md
@@ -27,6 +27,14 @@ Click [here](https://github.com/StephenGrider/ReactStarter/releases) then downlo
 # React学习笔记
 第一步 删除src文件，新建一个src文件，新建index.js
 
-
+//const 和var相同，声明一个变量，但是不能更改，例如，不能够再写上App = 5；
+```
+const App = function(){
+  return <div>Hi!</div>;
+}
+```
+上面的jsx语法实际上是简化代码的书写，webpack+babel将jsx代码转换为浏览器能够识别的普通js代码。
+本质上通过[babel](https://babeljs.io/repl)可以看到jsx代码转换后的js代码
 ![image](https://github.com/dreamerjackson/ReduxSimpleStarter/blob/part-1-JSX/1.png)
+
 第二步：
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+//导入react模块
+import React from 'react';
+
+//导入react-dom，目的是将component提交到页面上
+import ReactDom from 'react-dom';
+
 //create a new component .this component should produce some html
+//const 和var相同，声明一个变量，但是不能更改，例如，不能够再写上App = 5；
 
-const App = function(){
-
+// const App = function(){
+//   return <div>Hi!</div>;
+// }
+//替换为es6，匿名函数
+const App = ()=>{
   return <div>Hi!</div>;
 }
+
+
 //Take this component's generateed HTML and put it on the page(in the DOM)
+//当我们创建一个component的时候，我们仅仅是创建了一个class，一个type，但是我们需要在ReactDom.render中传递instance
+//<App /> 创建一个instance，本质上是创建了一个React.createElement(App, null);但是我不用理会
+//document.querySelector('.container')，找到class为container的标签，将isntance插入其中。
+ReactDom.render(<App />, document.querySelector('.container'));
